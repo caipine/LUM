e169758f24f64b4a8c16885ed629142c23a33391
caipine <caipine@users.noreply.github.com>
2019-06-05 11:26:58 -0500

    Update 2019-05-09.ava.R

--- 2019-05-09.ava.R
+++ 2019-05-09.ava.R
@@ -1,13 +1,15 @@
+setwd("Y:/WL/R")
 library('xlsx')
-
 pdf_file <- "2019-05-09-6965-673-7367.pdf"
-
 treatment <- c("DMSO", "7367", "BMN673", "BMN673-2")
 file1 <- "2019-05-09-6965-673-7367.xlsx"
-Sheet1 <-  read.xlsx(file1, sheetName = "Sheet1")
+conc <-  c(0,0.46875,0.9375,1.875,3.75,7.5,15,30)
 
 
-x <- Sheet1
+
+
+x<-  read.xlsx(file1, sheetName = "Sheet1")
+
 x1 <- x[1:8,2:4]
 x1$treatment <- treatment[1]
 x2 <- x[1:8,5:7]
@@ -21,7 +23,7 @@ x4$treatment <- treatment[4]
 colnames(x4) <- colnames(x1)
 
 y <-data.frame( rbind(x1,x2,x3,x4))
-y$con <-as.factor( c(0,0.46875,0.9375,1.875,3.75,7.5,15,30))
+y$con <- conc
 y$mean <- rowMeans(y[,1:3])
 
 y2 <- y[,1:3]
@@ -37,7 +39,7 @@ y_total <- y
 
 
 
-for (i in 2:9 ) {
+for (i in 2:8 ) {
 x <-  read.xlsx(file1, sheetName = paste("Sheet",i, sep=''))
 
 
@@ -54,7 +56,7 @@ x4$treatment <- treatment[4]
 colnames(x4) <- colnames(x1)
 
 y <-data.frame( rbind(x1,x2,x3,x4))
-y$con <-as.factor( c(0,0.46875,0.9375,1.875,3.75,7.5,15,30))
+y$con <- conc
 y$mean <- rowMeans(y[,1:3])
 
 y2 <- y[,1:3]
@@ -72,18 +74,29 @@ y_total <- rbind(y_total, y)
 
 y_total
 
-
-
 library(ggplot2)
-pdf(pdf_file,width=20,height=10)
+pdf(paste("color", pdf_file),width=20,height=10)
 ggplot(y_total,aes(x=con,y=mean,colour=treatment,group=treatment)) + 
   geom_errorbar(aes(ymin=mean-SD, 
                     ymax=mean+SD), width =.5)  +
   geom_line(aes(y = mean, group = treatment))  +
   xlab("Concentration") + 
   ylab("LUM") + 
-  theme(strip.text = element_text(size=25)) +
+ scale_x_continuous("AVA concentration",  sec.axis = sec_axis(~./15, name = "BMN673 concentration")) +
+theme(strip.text = element_text(size=25)) +
 facet_wrap( ~ cell , scales = "free_y", ncol = 4)
+
 dev.off()
 
 
+pdf(paste("line", pdf_file),width=20,height=10)
+ggplot(y_total,aes(x=con,y=mean,linetype=treatment, group=treatment)) + 
+  geom_errorbar(aes(ymin=mean-SD, 
+                    ymax=mean+SD), width =.5)  +
+  geom_line(aes(y = mean, group = treatment))  +
+  xlab("Concentration") + 
+  ylab("LUM") + 
+ scale_x_continuous("AVA concentration",  sec.axis = sec_axis(~./15, name = "BMN673 concentration")) +
+  theme(strip.text = element_text(size=25)) +
+  facet_wrap( ~ cell , scales = "free_y", ncol = 4)
+dev.off()
